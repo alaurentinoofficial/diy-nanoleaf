0f570ff040de461b8324aa5afa4250f65303cc97
Anderson Laurentino <anderson@laurentino.me>
2021-02-22 09:46:31 -0300

    Added the Storage manager with EEPROM

--- src/ConfigManager.h
+++ src/ConfigManager.h
@@ -1,4 +1,5 @@
 #include "Definitions.h"
+#include "Storage.h"
 
 #ifndef ConfigManager_h
 #define ConfigManager_h
@@ -6,13 +7,27 @@
 class ConfigManager
 {
 private:
+    Storage *storage;
     Configs configs;
 
 public:
-    ConfigManager() {}
-    bool begin() { return false; }
-    void save() {}
-    void load() {}
+    ConfigManager() {
+        storage = new Storage(512);
+    }
+    bool begin() {
+        if(storage->get(0) == 1) {
+            load();
+            return true;
+        }
+
+        return false;
+    }
+    void save() {
+        this->storage->write(1, &this->configs);
+    }
+    void load() {
+        this->storage->read(1, &this->configs);
+    }
     Configs &get()
     {
         return this->configs;
