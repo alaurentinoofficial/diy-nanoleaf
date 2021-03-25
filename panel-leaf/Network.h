01bdbb040b751893d94b3a829345d267ae3db859
Anderson Laurentino <anderson@laurentino.me>
2021-03-25 19:57:13 -0300

    Create implementation for debug network on computer

--- panel-leaf/Network.h
+++ panel-leaf/Network.h
@@ -3,6 +3,7 @@
 #ifndef Network_h
 #define Network_h
 
+#ifdef Arduino_h
 class EnlaceLayer
 {
 public:
@@ -54,6 +55,60 @@ public:
       Serial.read();
   }
 };
+#endif
+
+#ifndef Arduino_h
+#import <iostream>
+
+using namespace std;
+
+class EnlaceLayer
+{
+public:
+  static void begin(long speed)
+  {
+    cout << "Set Serial Rate> " << speed << "hz" << endl;
+  }
+
+  static int available()
+  {
+    return Serial.available();
+  }
+
+  static unsigned int writeByte(char value)
+  {
+    cout << value << " ";
+
+    return 1;
+  }
+
+  static unsigned int writeBytes(char *payload, int size_d)
+  {
+    unsigned int i;
+    for (i = 0; i < size_d; i++)
+    {
+      writeByte(payload[i]);
+    }
+
+    return i;
+  }
+
+  static void readBytesUntil(char lastChar, char *b)
+  {
+    Serial.readBytesUntil(lastChar, b, sizeof(b));
+  }
+
+  static char readByte()
+  {
+    return Serial.read();
+  }
+
+  static void close()
+  {
+    writeByte("<");
+  }
+};
+#endif
 
 class TransportLayer
 {
